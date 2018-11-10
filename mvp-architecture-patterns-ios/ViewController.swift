c02e176cab8d5e6c0ab4e16b450a0b948531decf
Juan Gerardo Cruz <kenetch1331@juans-mac-mini.local>
2018-11-10 07:06:06 -0600

    Initial Commit

---
+++ mvp-architecture-patterns-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mvp-architecture-patterns-ios
+//
+//  Created by Juan Gerardo Cruz on 11/10/18.
+//  Copyright © 2018 Juan Gerardo Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
